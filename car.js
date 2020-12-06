aeaf0203bc0673503bf0e82edf4492a56387519f
Vaibhavgambhir1467 <72294618+Vaibhavgambhir1467@users.noreply.github.com>
2020-12-06 15:10:44 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,28 @@
+class Car{
+constructor(x,y,width,angle){
+    var options={
+'friction':0.8,
+'density':1.8,
+    }
+    this.body=Bodies.rectangle(x,y,width,height,options);
+    this.width=width;
+    this.height=height;
+    World.add(world,this.body);
+}
+display(){
+var angle = this.body.angle;
+push();
+translate(this.body.position.x,this.body.position.y);
+rotate(angle);
+imageMode(CENTER);
+image(this.image,0,0,this.width,this,height);
+pop();
+
+
+
+
+
+}
+
+
+}
